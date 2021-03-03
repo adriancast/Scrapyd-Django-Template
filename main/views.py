9803078a2a3c8b58727dc1d94bc5453abcb6c860
Rekoc <rekoc13@gmail.com>
2021-03-03 10:44:10 +0100

    Update views.py

--- main/views.py
+++ main/views.py
@@ -80,7 +80,7 @@ def crawl(request):
         if status == 'finished':
             try:
                 # this is the unique_id that we created even before crawling started.
-                item = ScrapyItem.objects.get(unique_id=unique_id)
+                item = Quote.objects.get(unique_id=unique_id)
                 return JsonResponse({'data': item.to_dict['data']})
             except Exception as e:
                 return JsonResponse({'error': str(e)})
